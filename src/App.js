2470cf1372caaed5b8729cbc7f11f460b82f9dec
trucanh <anhtructran111@gmail.com>
2021-09-02 19:30:34 +0700

    learning styled-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  font-weight: bold;
+  height: ${(height)=> height.height === 10 ? "10px" : "20px"};cd
+`;
+
+
 
 const App = () => {
   const [value, setValue] = useState('abc')
@@ -10,11 +18,12 @@ const App = () => {
     setValue(value);
   }
 
+  const a = 10;
   return (
-    <div className="App">
+    <Wrap height={a}>
       <form><input type="input" onChange={getValue} /></form>
       {value}
-    </div>
+    </Wrap>
   );
 }
 
