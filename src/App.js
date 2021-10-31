55c71faf08e1e9134833934dd804f43e19d4c279
trucanh <anhtructran111@gmail.com>
2021-10-31 12:20:34 +0700

    review use state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+const gifts = [
+  "CPU i9",
+  "RAM 32GB RGB",
+  "RGB Keyboard",
+  "Laptop"
+]
 
 const App = () => {
-  const [counter, setCounter] = useState(1)
+  const [gift, setGift] = useState()
 
-  const handleIncrease = () => {
-    setCounter(counter + 1)
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+
+    setGift(gifts[index]);
   }
 
   return (
     <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+      <h1>{gift || "Chưa có phần thưởng"}</h1>
+      <button onClick={randomGift}>Lấy thưởng</button>
     </div>
   );
 }
